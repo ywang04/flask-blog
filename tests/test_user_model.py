f06ddd9cd6e5995bde9afc2469c99c5fa5caf22b
Yang <yang.wang04@gmail.com>
2016-07-26 20:38:28 +1000

    Password hashing tests

---
+++ tests/test_user_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env
+# coding:utf-8
+"""
+Created on 26/07/2016 14:37
+
+__author__ = 'Yang'
+__version__= '1.0'
+
+"""
+
+import unittest
+from app.models import User
+
+class UserModelTestCase(unittest.TestCase):
+    def test_password_setter(self):
+        pass
