ac263873c9a67c1d676fe8d66d66d5dc72d4da7d
Yang <yang.wang04@gmail.com>
2016-07-29 10:15:32 +1000

    User registration route

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,7 +12,8 @@ from flask import render_template,redirect,request,url_for,flash
 from flask.ext.login import login_user,login_required,logout_user
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm,RegistrationForm
+from .. import db
 
 
 @auth.route('/login',methods=['GET','POST'])
@@ -27,10 +28,22 @@ def login():
     return render_template('auth/login.html',form = form)
 
 
-
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html',form=form)
