387c07e73576099a4e20decb13fd07c794bf04d5
Yang <yang.wang04@gmail.com>
2016-07-29 16:28:03 +1000

    User account confirmation

--- app/models.py
+++ app/models.py
@@ -8,8 +8,11 @@ __version__= '1.0'
 
 """
 from werkzeug.security import generate_password_hash,check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from flask.ext.login import UserMixin
 from . import db
+from . import login_manager
 
 
 class Role(db.Model):
@@ -22,14 +25,14 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64),unique=True,index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
+    confirmed = db.Column(db.BOOLEAN,default=False)
 
     @property
     def password(self):
@@ -42,5 +45,29 @@ class User(db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
+    def generate_confirmation_token(self,expiration = 3600):
+        s = Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({'confirm':self.id})
+
+    def confirm(self,token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.load(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
