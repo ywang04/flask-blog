1d209e2e2e3d9bec4a85bc6e59f650fee4cee34d
Yang <yang.wang04@gmail.com>
2016-08-04 11:23:44 +1000

    Create profile page route

--- app/main/views.py
+++ app/main/views.py
@@ -8,11 +8,20 @@ __version__= '1.0'
 
 """
 
-from flask import render_template
+from flask import render_template,abort
 from . import main
+from ..models import User
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html',user=user)
 
