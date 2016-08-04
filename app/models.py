f5a6b476176cb5428a0015933410064a8f4c85b4
Yang <yang.wang04@gmail.com>
2016-08-04 11:07:29 +1000

    Add User information fields and refresh last visit time of a user

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ __author__ = 'Yang'
 __version__= '1.0'
 
 """
+from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
@@ -66,17 +67,21 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.BOOLEAN,default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(),default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
 
-    def __init__(self,**kwargs):
-        super(User,self).__init__(**kwargs)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
         if self.role is None:
             if self.email == current_app.config['FLASKY_ADMIN']:
-                print Role.query.filter_by(permissions=0xff).first()
                 self.role = Role.query.filter_by(permissions=0xff).first()
-            # if self.role is None:
-            #     self.role = Role.query.filter_by(default=True).first()
             else:
                 self.role = Role.query.filter_by(default=True).first()
+
 
     @property
     def password(self):
@@ -149,6 +154,10 @@ class User(UserMixin,db.Model):
     #to check for administration permissions
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
     def __repr__(self):
         return '<User %r>' % self.username
