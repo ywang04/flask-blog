c934048f96199e176ba0b84b504f04311ac7d8ac
Yang <yang.wang04@gmail.com>
2016-08-05 23:03:56 +1000

    Create Administrator-Level Profile Editor

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,8 +9,9 @@ __version__= '1.0'
 """
 
 from flask.ext.wtf import Form
-from wtforms import StringField,SubmitField,TextAreaField
-from wtforms.validators import Required,Length
+from wtforms import StringField,SubmitField,TextAreaField,BooleanField,SelectField,ValidationError
+from wtforms.validators import Required,Length,Email,Regexp
+from ..models import Role,User
 
 class NameForm(Form):
     name = StringField('What is your name?',validators=[Required()])
@@ -23,6 +24,44 @@ class EditProfile(Form):
     location = StringField('Location',validators=[Length(0,64)])
     about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(Form):
+    email = StringField('Email',validators=[Required(),Length(1,64),
+                                            Email()])
+    username = StringField('Username',validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+
+    confirmed = BooleanField('Confirmed')
+    #SelectField is a dropdown list in the form
+    role = SelectField('Role',coerce=int)
+    name = StringField('Real name', validators=[Length(0,64)])
+    location = StringField('Location',validators=[Length(0,64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+    def __init__(self,user,*args,**kwargs):
+        super(EditProfileAdminForm,self).__init__(*args,**kwargs)
+        #choices is the role's attribute while role is the instance of SelectField
+        self.role.choices = [(role.id,role.name) for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self,field):
+        if field.data != self.user.email and \
+            User.query.filter_by(email= field.data).first():
+            raise ValidationError('Email already registered.')
+
+
+    def validate_username(self,field):
+        if field.data != self.user.username and \
+            User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+
+
+
+
 
 
 
--- app/main/views.py
+++ app/main/views.py
@@ -11,9 +11,10 @@ __version__= '1.0'
 from flask import render_template,abort,redirect,url_for,flash
 from flask.ext.login import current_user,login_required
 from . import main
-from ..models import User
-from .forms import EditProfile
+from ..models import User,Role
+from .forms import EditProfile,EditProfileAdminForm
 from .. import db
+from ..decorators import admin_required
 
 @main.route('/')
 def index():
@@ -44,6 +45,35 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html',form=form)
+
+
+
+@main.route('/edit-profile/<int:id>',methods=['GET','POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('main.user',username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    #should be role_id instead of role
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html',form=form,user=user)
 
 
 @main.route('/about')
