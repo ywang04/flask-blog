4461eef2156b4ae18154d2e7e476c8ad5b6ddb3c
Yang <yang.wang04@gmail.com>
2016-08-12 19:29:35 +1000

    Permanent links to posts

--- app/main/views.py
+++ app/main/views.py
@@ -97,6 +97,10 @@ def edit_profile_admin(id):
     form.about_me.data = user.about_me
     return render_template('edit_profile.html',form=form,user=user)
 
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html',posts=[post])
 
 @main.route('/about')
 def about():
