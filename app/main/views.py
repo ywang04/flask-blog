b27b356eb92cad8af19a14dff912d17aaf72f76e
Yang <yang.wang04@gmail.com>
2016-08-23 10:51:32 +1000

    Implement Comment function

--- app/main/views.py
+++ app/main/views.py
@@ -11,8 +11,8 @@ __version__= '1.0'
 from flask import render_template,abort,redirect,url_for,flash,request,current_app
 from flask.ext.login import current_user,login_required
 from . import main
-from ..models import User,Role,Permission,Post,Category
-from .forms import EditProfile,EditProfileAdminForm,PostForm,AddCategory
+from ..models import User,Role,Permission,Post,Category,Comment
+from .forms import EditProfile,EditProfileAdminForm,PostForm,AddCategory,CommentForm
 from .. import db
 from ..decorators import admin_required
 
@@ -91,10 +91,26 @@ def edit_profile_admin(id):
     form.about_me.data = user.about_me
     return render_template('edit_profile.html',form=form,user=user)
 
-@main.route('/post/<int:id>')
+@main.route('/post/<int:id>',methods=['GET','POST'])
 def post(id):
     post = Post.query.get_or_404(id)
-    return render_template('post.html',post=post)
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(body=form.body.data,
+                          post=post,
+                          author=current_user._get_current_object())
+        db.session.add(comment)
+        flash('Your comment has been published.')
+        return redirect(url_for('main.post',id=post.id,page=-1))
+    page = request.args.get('page',1,type=int)
+    if page == -1:
+        page = (post.comments.count() - 1) // \
+            current_app.config['YUPRA_COMMENTS_PER_PAGE'] + 1
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page,per_page= current_app.config['YUPRA_COMMENTS_PER_PAGE'],
+        error_out = False)
+    comments = pagination.items
+    return render_template('post.html',posts=[post],form=form,comments=comments,pagination=pagination)
 
 @main.route('/new-post',methods=['GET','POST'])
 @login_required
