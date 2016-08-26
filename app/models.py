95350f5c75f7682a20b1bad30df5f459efe5212a
Yang <yang.wang04@gmail.com>
2016-08-26 18:46:58 +1000

    Fix Follow module to add primary_key

--- app/models.py
+++ app/models.py
@@ -74,7 +74,7 @@ class Follow(db.Model):
     follower_id = db.Column(db.Integer,db.ForeignKey('users.id'),
                             primary_key=True)
     followed_id = db.Column(db.Integer,db.ForeignKey('users.id'),
-                            primary_key=True
+                            primary_key=True)
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
 
 
