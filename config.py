9f7a3a12544c34fe96114b9d0e3329659797cb42
Yang <yang.wang04@gmail.com>
2016-10-12 21:06:51 +1100

    Change  YUORA_MAIL_SUBJECT_PREFIX to  'Welcome to Yuora!'

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    YUORA_MAIL_SUBJECT_PREFIX = '[Welcome to Yuora]'
+    YUORA_MAIL_SUBJECT_PREFIX = 'Welcome to Yuora!'
     YUORA_MAIL_SENDER = 'Yuora Admin <yuora@example.com>'
     YUORA_ADMIN = os.environ.get('Yuora_ADMIN')
     YUORA_POSTS_PER_PAGE = 10
