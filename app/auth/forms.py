187c479657dbab72b0c0d5443973e34aa361487e
Yang <yang.wang04@gmail.com>
2016-10-16 15:01:41 +1100

    Change attributes of Login and Register Form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -19,19 +19,19 @@ from ..models import User
 # images = UploadSet('images', IMAGES)
 
 class LoginForm(Form):
-    email = StringField('Email address',validators=[DataRequired(),Length(1,64),Email()])
+    email = StringField('Email Address',validators=[DataRequired(),Length(1,64),Email()])
     password = PasswordField('Password',validators=[DataRequired()])
     remember_me = BooleanField('Stay signed in')
-    submit = SubmitField('Log In')
+    submit = SubmitField('Sign In')
 
 
 class RegistrationForm(Form):
-    email = StringField('Email address',validators=[DataRequired(),Length(1,64),Email()])
     username = StringField('Username',validators=[DataRequired(),Length(1,64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,
                                                                                  'Usernames must have only letters, '
                                                                                  'numbers, dots or underscores')])
+    email = StringField('Email Address', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password',validators=[DataRequired(),EqualTo('password2',message='Passwords must match.')])
-    password2 = PasswordField('Re-enter password',validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password',validators=[DataRequired()])
     submit = SubmitField('Sign Up Now',render_kw={"class":"btn btn-primary btn-block"})
 
 
